d53bfce0d441c8b8b48760ee3cbbd2f70e815c07
abd-005 <abd.me7@gmail.com>
2025-10-09 21:24:34 +0600

    basic added commit

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,23 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
+import Products from "../Pages/Products";
+import Home from "../Pages/Home";
+import MainLayout from "../Layouts/MainLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <MainLayout />,
+    children : [
+        {
+        //   path: "/", // one way
+        index: true, //alt way
+          element: <Home />,
+        },
+        {
+          path: '/products',
+          element: <Products></Products>
+        }   
+    ]
   },
 ]);
