2ad0018eee23aac1ac4e45457d3a94fd88cf854b
abd-005 <abd.me7@gmail.com>
2025-10-09 22:06:17 +0600

    Navbar added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Home = () => {
+    // console.log(odfgoipkdmfdg)
     return (
-        <div>
+        <div >
             Home
         </div>
     );
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,11 +3,14 @@ import App from "../App";
 import Products from "../Pages/Products";
 import Home from "../Pages/Home";
 import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+import Wishlist from "../Pages/Wishlist";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement : <ErrorPage />,
     children : [
         {
         //   path: "/", // one way
@@ -17,7 +20,16 @@ export const router = createBrowserRouter([
         {
           path: '/products',
           element: <Products></Products>
+        },
+        {
+          path: '/wishlist',
+          element: <Wishlist></Wishlist>
         }   
     ]
   },
+//   {
+//     //old way | work for undefined path
+//     path:'*',
+//     element: <ErrorPage/>
+//   }
 ]);
